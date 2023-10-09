e3104187b150f19fc566003c2b935b01a83290a6
Дениев Халид Джамалайлаевич <hdeniev@rtk-element.ru>
2023-10-10 01:48:30 +0300

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import bodyParser from "body-parser";
 import { getEnv } from "@/utils/env";
 import { createRoutes } from "@/lib/createRoutes";
 import { initDatabase } from "@/lib/database";
@@ -10,6 +11,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+app.use(bodyParser.json({ limit: "35mb" }));
+app.use(
+  bodyParser.urlencoded({
+    limit: "35mb",
+    parameterLimit: 50000,
+  })
+);
 
 createRoutes(app);
 
