2a0612a93b33151e9514258163b3c462abcb828c
Дениев Халид Джамалайлаевич <hdeniev@rtk-element.ru>
2023-10-15 05:28:41 +0300

    commit

--- src/lib/multer.ts
+++ src/lib/multer.ts
@@ -4,10 +4,14 @@ import * as process from "process";
 const UPLOADS_FOLDER = `${process.cwd()}/src/uploads`;
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
+  destination: function (_, file: Express.Multer.File, cb) {
     cb(null, UPLOADS_FOLDER);
   },
-  filename: function (req, file, cb) {
+  filename: function (
+    req,
+    file,
+    cb: (error: Error | null, filename: string) => void
+  ) {
     cb(null, Date.now() + " - " + file.originalname);
   },
 });
