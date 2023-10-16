7861ead62f9b7cb361a404e2b79a3e7d6fc54738
Дениев Халид Джамалайлаевич <hdeniev@rtk-element.ru>
2023-10-16 13:07:05 +0300

    commit

--- src/controllers/ad/validation.ts
+++ src/controllers/ad/validation.ts
@@ -2,7 +2,6 @@ import * as yup from "yup";
 export const createFormSchema = yup
   .object({
     title: yup.string().required("Обязятельное поле"),
-    weight: yup.number().required("Обязятельное поле"),
     date: yup.date().required("Обязятельное поле"),
     files: yup.array().required("Обязятельное поле"),
   })
