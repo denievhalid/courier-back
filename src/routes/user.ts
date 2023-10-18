f97cde02a7fe79a1e306dc14260b855eb6b29c22
Дениев Халид Джамалайлаевич <hdeniev@rtk-element.ru>
2023-10-18 11:05:20 +0300

    commit

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { create, me } from "@/controllers/user";
+import { authenticate } from "@/middlewares/authenticate";
 
 const router = Router();
 
 router.post("/", create);
-router.get("/me", me);
+router.get("/me", authenticate, me);
 
 export default router;
