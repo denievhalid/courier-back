74a4b84439b96097a71f0106eb659db0866667c5
Дениев Халид Джамалайлаевич <hdeniev@rtk-element.ru>
2023-10-18 10:05:37 +0300

    commit

--- src/controllers/favorite/index.ts
+++ src/controllers/favorite/index.ts
@@ -4,13 +4,24 @@ import { StatusCodes } from "http-status-codes";
 import type { Request, Response } from "express";
 import { getService } from "@/lib/container";
 import { getParam } from "@/utils/getParam";
+import { PipelineStage } from "mongoose";
 
 export const getList = asyncHandler(async (req: Request, res: Response) => {
   const user = getParam(req, "user");
 
   const favoriteService = getService("favorite");
 
-  const data = await favoriteService.getList();
+  const query: PipelineStage[] = [
+    {
+      $match: {
+        $expr: {
+          $eq: ["$user", { $toObjectId: user }],
+        },
+      },
+    },
+  ];
+
+  const data = await favoriteService.getList(query);
 
   return getResponse(res, { data }, StatusCodes.OK);
 });
