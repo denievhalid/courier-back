c6b73ea7112a8ef1a5e66ce7e54b1bb85b473370
Дениев Халид Джамалайлаевич <hdeniev@rtk-element.ru>
2023-10-19 02:26:55 +0300

    commit

--- src/controllers/ad/index.ts
+++ src/controllers/ad/index.ts
@@ -67,7 +67,7 @@ export const getList = asyncHandler(async (req: Request, res: Response) => {
   query.push(getProjectPipeline());
   query.push(getAddFieldsPipeline());
   query.push(getLimitPipeline(LIMIT));
-
+  console.log(query);
   const data = await getService("ad").getList(query);
 
   return getResponse(res, { data });
--- src/controllers/ad/utils.ts
+++ src/controllers/ad/utils.ts
@@ -19,9 +19,8 @@ export const getMatchPipeline = (match: Record<string, any>) => {
 
   _.forEach(match, (item) => {
     const [param, value] = parseMatchParam(item);
-    console.log(value);
+
     if (isValidObjectId(value)) {
-      console.log(value);
       stage["$match"]["$expr"] = {
         $eq: [`$${param}`, { $toObjectId: value }],
       };
