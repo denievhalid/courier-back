c2ff0f6ab7d8a46f9d1582c71c2e49aa8e5955de
Дениев Халид Джамалайлаевич <hdeniev@rtk-element.ru>
2023-10-18 04:09:56 +0300

    commit

--- src/controllers/favorite/index.ts
+++ src/controllers/favorite/index.ts
@@ -1,7 +1,16 @@
 import { asyncHandler } from "@/utils/asyncHandler";
 import { getResponse } from "@/utils/getResponse";
+import { StatusCodes } from "http-status-codes";
 import type { Request, Response } from "express";
+import { getService } from "@/lib/container";
+import { getParam } from "@/utils/getParam";
 
 export const getList = asyncHandler(async (req: Request, res: Response) => {
-  return getResponse(res, {});
+  const user = getParam(req, "user");
+
+  const favoriteService = getService("favorite");
+
+  const data = await favoriteService.getList();
+
+  return getResponse(res, { data }, StatusCodes.OK);
 });
