c72fc4adeda90d2b3cb0eb5b4e327c857972b26a
Дениев Халид Джамалайлаевич <hdeniev@rtk-element.ru>
2023-10-23 18:11:52 +0300

    commit

--- src/services/message.ts
+++ src/services/message.ts
@@ -1,6 +1,11 @@
 import { getModel } from "@/lib/container";
 import type { MessageType } from "@/types";
+import { PipelineStage } from "mongoose";
 
 export const create = (payload: MessageType) => {
   return getModel("message").create(payload);
+};
+
+export const aggregate = (pipeline: PipelineStage[]) => {
+  return getModel("message").aggregate(pipeline);
 };
