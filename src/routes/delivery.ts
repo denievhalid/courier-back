669cc6c94556234395c419b770d9f4f5432a654d
Дениев Халид Джамалайлаевич <hdeniev@rtk-element.ru>
2023-11-01 19:23:08 +0300

    commit

--- src/routes/delivery.ts
+++ src/routes/delivery.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { create } from "@/controllers/delivery";
+import { create, update } from "@/controllers/delivery";
 import { authenticate } from "@/middlewares/authenticate";
 
 const router = Router();
 
 router.post("/", authenticate, create);
+router.patch("/", authenticate, update);
 
 export default router;
