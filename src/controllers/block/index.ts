3b2ade1c3d3c787b133a8da1b4d461f6e5364e40
Дениев Халид Джамалайлаевич <hdeniev@rtk-element.ru>
2023-11-04 03:31:25 +0300

    commit

--- src/controllers/block/index.ts
+++ src/controllers/block/index.ts
@@ -11,17 +11,17 @@ export const create = asyncHandler(async (req: Request, res: Response) => {
   const user = getParam(req, "user") as UserType;
   const blockedUser = getParam(req.body, "user");
 
-  const blockedService = getService("block");
+  const blockService = getService("block");
 
   const payload = {
     blockedUser: toObjectId(blockedUser._id),
     user: toObjectId(user._id),
   };
 
-  const count = await blockedService.count(payload);
+  const count = await blockService.count(payload);
 
   if (!count) {
-    await blockedService.create({
+    await blockService.create({
       blockedUser: toObjectId(blockedUser._id),
       user: toObjectId(user._id),
     });
@@ -42,17 +42,17 @@ export const remove = asyncHandler(async (req: Request, res: Response) => {
   const user = getParam(req, "user") as UserType;
   const blockedUser = getParam(req.body, "user");
 
-  const blockedService = getService("block");
+  const blockService = getService("block");
 
   const payload = {
     blockedUser: toObjectId(blockedUser._id),
     user: toObjectId(user._id),
   };
 
-  const count = await blockedService.count(payload);
+  const count = await blockService.count(payload);
 
   if (count) {
-    await blockedService.remove({
+    await blockService.remove({
       blockedUser: toObjectId(blockedUser._id),
       user: toObjectId(user._id),
     });
