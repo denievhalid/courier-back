a2bd137445183d0f0ccdaad2851a520a5defd4e8
Дениев Халид Джамалайлаевич <hdeniev@rtk-element.ru>
2023-11-07 22:27:39 +0300

    commit

--- src/services/direction.ts
+++ src/services/direction.ts
@@ -1,6 +1,7 @@
 import { getModel } from "@/lib/container";
 import type { FilterQuery, PipelineStage } from "mongoose";
 import type { DirectionType } from "@/types";
+import { FavoriteType } from "@/types";
 
 export const create = (payload: DirectionType) => {
   return getModel("direction").create(payload);
@@ -8,6 +9,10 @@ export const create = (payload: DirectionType) => {
 
 export const count = (payload: DirectionType) => {
   return getModel("direction").countDocuments(payload);
+};
+
+export const remove = (filter: FilterQuery<DirectionType>) => {
+  return getModel("direction").findOneAndRemove(filter);
 };
 
 export const findOne = (filter: FilterQuery<DirectionType>) => {
