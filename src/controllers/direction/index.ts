5f1fc2916bfc63600b6b8b59f6c15116609e6807
Дениев Халид Джамалайлаевич <hdeniev@rtk-element.ru>
2023-11-08 05:12:44 +0300

    commit

--- src/controllers/direction/index.ts
+++ src/controllers/direction/index.ts
@@ -5,6 +5,7 @@ import { StatusCodes } from "http-status-codes";
 import { getParam } from "@/utils/getParam";
 import { toObjectId } from "@/utils/toObjectId";
 import { getService } from "@/lib/container";
+import { UserType } from "@/types";
 
 export const create = asyncHandler(async (req: Request, res: Response) => {
   const hash = getParam(req.body, "hash");
@@ -26,12 +27,24 @@ export const create = asyncHandler(async (req: Request, res: Response) => {
 });
 
 export const getList = asyncHandler(async (req: Request, res: Response) => {
-  return getResponse(res, {}, StatusCodes.OK);
+  const user = getParam(req, "user") as UserType;
+
+  const directionService = getService("direction");
+
+  const data = await directionService.aggregate([
+    {
+      $match: {
+        user: toObjectId(user._id),
+      },
+    },
+  ]);
+
+  return getResponse(res, { data }, StatusCodes.OK);
 });
 
 export const remove = asyncHandler(async (req: Request, res: Response) => {
   const hash = getParam(req.body, "hash");
-  const user = getParam(req, "user");
+  const user = getParam(req, "user") as UserType;
 
   const payload = { hash, user: toObjectId(user._id) };
 
