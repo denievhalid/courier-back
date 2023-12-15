f057f125166c591aa7c59e4bccc0dd48b4b67b5b
Дениев Халид Джамалайлаевич <hdeniev@rtk-element.ru>
2023-12-15 21:35:40 +0300

    fix

--- src/services/message.ts
+++ src/services/message.ts
@@ -1,18 +1,26 @@
 import { getModel } from "@/lib/container";
 import type { MessageType } from "@/types";
-import { FilterQuery, PipelineStage } from "mongoose";
+import { FilterQuery, PipelineStage, UpdateQuery } from "mongoose";
+import { Services, UserType } from "@/types";
 
 export const create = (payload: MessageType) => {
-  return getModel("message").create(payload);
+  return getModel(Services.MESSAGE).create(payload);
 };
 
 export const aggregate = (pipeline: PipelineStage[]) => {
-  return getModel("message").aggregate(pipeline);
+  return getModel(Services.MESSAGE).aggregate(pipeline);
 };
 export const find = (filter: FilterQuery<MessageType>) => {
-  return getModel("message").find(filter);
+  return getModel(Services.MESSAGE).find(filter);
 };
 
 export const findOne = (filter: FilterQuery<MessageType>) => {
-  return getModel("message").findOne(filter);
+  return getModel(Services.MESSAGE).findOne(filter);
+};
+
+export const update = (
+  filter: FilterQuery<UserType>,
+  update: UpdateQuery<UserType>
+) => {
+  return getModel(Services.MESSAGE).findOneAndUpdate(filter, update);
 };
