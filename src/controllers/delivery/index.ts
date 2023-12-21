83ed4552c3ac28e5afea726591277d39fab537ba
Дениев Халид Джамалайлаевич <hdeniev@rtk-element.ru>
2023-12-21 15:05:46 +0300

    commit

--- src/controllers/delivery/index.ts
+++ src/controllers/delivery/index.ts
@@ -127,10 +127,7 @@ export const remove = asyncHandler(async (req: Request, res: Response) => {
       SOCKET_EVENTS.UPDATE_DELIVERY_STATUS,
       null
     );
-    io.to(conversation?._id?.toString()).emit(
-      SOCKET_EVENTS.UPDATE_AD_COURIER,
-      user
-    );
+    io.to(conversation?.toString()).emit(SOCKET_EVENTS.UPDATE_AD_COURIER, user);
   }
 
   return getResponse(res, {}, StatusCodes.OK);
