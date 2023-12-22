61772ac40eb3132391732d4f017f93e837e0560b
nekoulahaddad <nekoulahaddad@gmail.com>
2023-12-22 22:20:05 +0300

    fixes

--- src/routes/delivery.ts
+++ src/routes/delivery.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { create, remove, update, getByAdId } from "@/controllers/delivery";
 import { authenticate } from "@/middlewares/authenticate";
+import { getAdMiddleware } from "@/controllers/delivery/middlewares";
 
 const router = Router();
 
-router.post("/", authenticate, create);
+router.post("/", authenticate, getAdMiddleware, create);
 router.get("/", authenticate, getByAdId);
-router.patch("/", authenticate, update);
+router.patch("/", authenticate, getAdMiddleware, update);
 router.delete("/:ad", authenticate, remove);
 
 export default router;
