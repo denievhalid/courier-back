da160f092fb3bc4d6a01481400b5b57c21683323
nekoulahaddad <nekoulahaddad@gmail.com>
2023-12-25 19:09:14 +0300

    change database

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -2,5 +2,5 @@ import mongoose from "mongoose";
 import { getEnv } from "@/utils/env";
 
 export const initDatabase = async () => {
-  return mongoose.connect(getEnv("mongoose_uri"));
+  return mongoose.connect(getEnv("MONGOOSE_URI_WITH_REPLICA"));
 };
