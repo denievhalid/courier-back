acca56f41cc04d5c4303a708a1772561c3ff16bf
Дениев Халид Джамалайлаевич <hdeniev@rtk-element.ru>
2023-12-30 21:39:23 +0300

    comimt

--- src/middlewares/getUser.ts
+++ src/middlewares/getUser.ts
@@ -2,6 +2,9 @@ import { getParam } from "@/utils/getParam";
 import { asyncHandler } from "@/utils/asyncHandler";
 import { getService } from "@/lib/container";
 import { Services } from "@/types";
+import { getEnv } from "@/utils/env";
+import { getUserAggregate } from "@/utils/aggregate";
+import _ from "lodash";
 
 export const getUser = asyncHandler(async (req, res, next) => {
   const token = getParam(req, "token");
@@ -12,20 +15,22 @@ export const getUser = asyncHandler(async (req, res, next) => {
 
   const tokenService = getService(Services.TOKEN);
 
-  // const verified = tokenService.verify(token, getEnv("JWT_SECRET")) as {
-  //   phoneNumber: string;
-  // };
-  //
-  // if (!verified) {
-  //   return next();
-  // }
+  // @ts-ignore
+  const verified = tokenService.verify(token, getEnv("JWT_SECRET")) as {
+    phoneNumber: string;
+  };
 
-  //const user = await getUserAggregate(verified.phoneNumber);
+  if (!verified) {
+    return next();
+  }
 
-  // if (_.isEmpty(user)) {
-  //   return next();
-  // }
-  //
-  // _.set(req, "user", _.first(user));
-  // return next();
+  const user = await getUserAggregate(verified.phoneNumber);
+
+  if (_.isEmpty(user)) {
+    return next();
+  }
+
+  _.set(req, "user", _.first(user));
+
+  return next();
 });
