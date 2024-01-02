48c5cb6c49b424d37732567973de0b025edf00e1
nekoulahaddad <nekoulahaddad@gmail.com>
2024-01-03 01:49:45 +0300

    fix

--- src/routes/delivery.ts
+++ src/routes/delivery.ts
@@ -18,7 +18,7 @@ router.post(
   useSocket
 );
 router.get("/", authenticate, getByAdId);
-router.patch("/", authenticate, useGetAdMiddleware, update);
+router.patch("/", authenticate, useGetAdMiddleware, update, useSocket);
 router.delete("/:ad", authenticate, useGetConversationById, remove, useSocket);
 
 export default router;
