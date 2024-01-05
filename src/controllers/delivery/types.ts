65b3c51dd770240d801d0ecab184428c4a10ca8a
Дениев Халид Джамалайлаевич <hdeniev@rtk-element.ru>
2024-01-05 16:20:16 +0300

    commit

--- src/controllers/delivery/types.ts
+++
@@ -1,7 +0,0 @@
-import { AdType, DeliveryStatus, UserType } from "@/types";
-
-export type DeliveryFacadeProps = {
-  ad: AdType;
-  status: DeliveryStatus;
-  user: UserType;
-};
--- src/services/user.ts
+++ src/services/user.ts
@@ -1,9 +1,17 @@
 import { BaseService } from "@/services/base";
-import { getModel } from "@/lib/container";
-import { Models } from "@/types";
+import { getModel, getService } from "@/lib/container";
+import { DeliveryStatus, Models, Services, UserType } from "@/types";
+import { toObjectId } from "@/utils/toObjectId";
 
 export class UserService extends BaseService {
   constructor() {
     super(getModel(Models.USER));
+  }
+
+  async getDeliveriesCount(user: UserType) {
+    return getService(Services.DELIVERY).count({
+      status: DeliveryStatus.RECEIVED,
+      user: toObjectId(user._id),
+    });
   }
 }
